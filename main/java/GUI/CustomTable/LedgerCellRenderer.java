b5a78f4f13fe1899881961979bb2b7311d0388e7
pkmed <pkmed.dev@gmail.com>
2018-09-11 14:07:16 +0700

    fix row selection

--- main/java/GUI/CustomTable/LedgerCellRenderer.java
+++ main/java/GUI/CustomTable/LedgerCellRenderer.java
@@ -10,13 +10,19 @@ public class LedgerCellRenderer extends JTextArea implements TableCellRenderer {
         setWrapStyleWord(true);
     }
     //TODO: cell alignment
-    //TODO: row select on cell select
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         setText(value.toString());
         setSize(table.getColumnModel().getColumn(column).getWidth(), getPreferredSize().height);
         if (table.getRowHeight(row) != getPreferredSize().height) {
             table.setRowHeight(row, getPreferredSize().height);
+        }
+        if(isSelected){
+            setBackground(table.getSelectionBackground());
+            setForeground(table.getSelectionForeground());
+        } else {
+            setBackground(table.getBackground());
+            setForeground(table.getForeground());
         }
         return this;
     }
